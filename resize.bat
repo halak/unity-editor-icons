e157e9050a5559cbdeec29791cbcf91b631fc68e
Halak <halakshin@gmail.com>
2020-07-27 18:12:55 +0900

    Update resize.bat

--- resize.bat
+++ resize.bat
@@ -1,3 +1,5 @@
 @echo off
 
-FOR /R "icons\small" %%F in (.) DO ( "mogrify" -resize "32x32>" "%%F\*.png" )
+FOR /R "icons\small" %%F in (.) DO ( "mogrify" -filter Lanczos2 -resize "32x32>" -quality 95 -strip "%%F\*.png" )
+
+PAUSE
