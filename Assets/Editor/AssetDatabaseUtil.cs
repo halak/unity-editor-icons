a50a06c566dec1287c8534c0e9fc2e08665264e8
Ernest Suryś <ernest.surys@gmail.com>
2021-06-26 15:12:53 +0200

    Extracted more logic to AssetDatabaseUtil

---
+++ Assets/Editor/AssetDatabaseUtil.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEditor.Experimental;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Halak
+{
+    public static class AssetDatabaseUtil
+    {
+        public static readonly AssetBundle EditorAssetBundle = GetEditorAssetBundle();
+
+        private static readonly PropertyInfo InspectorModeInfo =
+            typeof(SerializedObject).GetProperty("inspectorMode", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        private const string FileIdFieldName = "m_LocalIdentfierInFile"; //note the misspelling!
+        public static readonly string IconsPath = GetIconsPath();
+
+        public static Texture2D[] GetAllEditorIcons()
+        {
+            const StringComparison comparison = StringComparison.OrdinalIgnoreCase;
+            return (from name in EditorAssetBundle.GetAllAssetNames()
+                where name.StartsWith(IconsPath, comparison)
+                where name.EndsWith(".png", comparison) || name.EndsWith(".asset", comparison)
+                let tex = EditorAssetBundle.LoadAsset<Texture2D>(name)
+                where tex != null
+                select tex).ToArray();
+        }
+        
+        private static AssetBundle GetEditorAssetBundle()
+        {
+            var editorGUIUtility = typeof(EditorGUIUtility);
+            var getEditorAssetBundle = editorGUIUtility.GetMethod(
+                "GetEditorAssetBundle",
+                BindingFlags.NonPublic | BindingFlags.Static);
+
+            return (AssetBundle) getEditorAssetBundle.Invoke(null, new object[] { });
+        }
+
+        public static long GetFileId(Object obj)
+        {
+#if UNITY_2018_1_OR_NEWER && false
+            AssetDatabase.TryGetGUIDAndLocalFileIdentifier(obj, out _, out long fileId);
+            return fileId;
+#else
+            var serializedObject = new SerializedObject(obj);
+            InspectorModeInfo.SetValue(serializedObject, InspectorMode.Debug);
+            return serializedObject.FindProperty(FileIdFieldName).longValue;
+#endif
+        }
+
+        public static string GetIconsPath()
+        {
+#if UNITY_2018_3_OR_NEWER
+            return EditorResources.iconsPath;
+#else
+            var assembly = typeof(EditorGUIUtility).Assembly;
+            var editorResourcesUtility = assembly.GetType("UnityEditorInternal.EditorResourcesUtility");
+
+            var iconsPathProperty = editorResourcesUtility.GetProperty(
+                "iconsPath",
+                BindingFlags.Static | BindingFlags.Public);
+
+            return (string)iconsPathProperty.GetValue(null, new object[] { });
+#endif
+        }
+    }
+}
--- Assets/Editor/IconMiner.cs
+++ Assets/Editor/IconMiner.cs
@@ -5,10 +5,8 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using UnityEditor;
-using UnityEditor.Experimental;
 using UnityEngine;
-using Object = UnityEngine.Object;
-
+using static Halak.AssetDatabaseUtil;
 namespace Halak
 {
     public static class IconMiner
@@ -19,10 +17,8 @@ namespace Halak
             EditorUtility.DisplayProgressBar("Export Icons", "Exporting...", 0.0f);
             try
             {
-                var editorAssetBundle = GetEditorAssetBundle();
-                var iconsPath = AssetDatabaseUtil.GetIconsPath();
                 var count = 0;
-                foreach (var (iconBundleName, icon) in GetAllIcons(editorAssetBundle, iconsPath))
+                foreach (var icon in GetAllEditorIcons())
                 {
                     ExportIcon("icons/original/", icon);
                     count++;
@@ -62,8 +58,6 @@ namespace Halak
             EditorUtility.DisplayProgressBar("Generate README.md", "Generating...", 0.0f);
             try
             {
-                var editorAssetBundle = GetEditorAssetBundle();
-                var iconsPath = AssetDatabaseUtil.GetIconsPath();
                 var readmeContents = new StringBuilder();
 
                 readmeContents.AppendLine("Unity Editor Built-in Icons");
@@ -82,10 +76,10 @@ namespace Halak
                 readmeContents.AppendLine("| Icon | Name | File ID |");
                 readmeContents.AppendLine("|------|------|---------|");
 
-                var icons = GetAllIcons(editorAssetBundle, iconsPath).ToArray();
+                var icons = GetAllEditorIcons().ToArray();
                 for (var i = 0; i < icons.Length; i++)
                 {
-                    var (iconName, iconTexture) = icons[i];
+                    var iconTexture = icons[i];
                     EditorUtility.DisplayProgressBar("Generate README.md",
                         $"Generating... ({i + 1}/{icons.Length})", (float) i / icons.Length);
 
@@ -109,59 +103,6 @@ namespace Halak
             }
         }
 
-        private static IEnumerable<(string name, Texture2D texture)> GetAllIcons(AssetBundle editorAssetBundle,
-            string iconsPath)
-        {
-            const StringComparison comparison = StringComparison.OrdinalIgnoreCase;
-            return from name in editorAssetBundle.GetAllAssetNames()
-                where name.StartsWith(iconsPath, comparison)
-                where name.EndsWith(".png", comparison) || name.EndsWith(".asset", comparison)
-                let tex = editorAssetBundle.LoadAsset<Texture2D>(name)
-                where tex != null
-                select (name, tex);
-        }
-
-        private static AssetBundle GetEditorAssetBundle()
-        {
-            var editorGUIUtility = typeof(EditorGUIUtility);
-            var getEditorAssetBundle = editorGUIUtility.GetMethod(
-                "GetEditorAssetBundle",
-                BindingFlags.NonPublic | BindingFlags.Static);
-
-            return (AssetBundle) getEditorAssetBundle.Invoke(null, new object[] { });
-        }
-    }
-
-    public static class AssetDatabaseUtil
-    {
-        private static readonly PropertyInfo InspectorModeInfo = typeof(SerializedObject).GetProperty("inspectorMode", BindingFlags.NonPublic | BindingFlags.Instance);
-        private const string FileIdFieldName ="m_LocalIdentfierInFile";  //note the misspelling!
-        public static long GetFileId(Object obj)
-        {
-#if UNITY_2018_1_OR_NEWER && false
-            AssetDatabase.TryGetGUIDAndLocalFileIdentifier(obj, out _, out long fileId);
-            return fileId;
-#else
-            var serializedObject = new SerializedObject(obj);
-            InspectorModeInfo.SetValue(serializedObject, InspectorMode.Debug);
-            return  serializedObject.FindProperty(FileIdFieldName).longValue;
-#endif
-        }
         
-        public static string GetIconsPath()
-        {
-#if UNITY_2018_3_OR_NEWER
-            return EditorResources.iconsPath;
-#else
-            var assembly = typeof(EditorGUIUtility).Assembly;
-            var editorResourcesUtility = assembly.GetType("UnityEditorInternal.EditorResourcesUtility");
-
-            var iconsPathProperty = editorResourcesUtility.GetProperty(
-                "iconsPath",
-                BindingFlags.Static | BindingFlags.Public);
-
-            return (string)iconsPathProperty.GetValue(null, new object[] { });
-#endif
-        }
     }
 }
